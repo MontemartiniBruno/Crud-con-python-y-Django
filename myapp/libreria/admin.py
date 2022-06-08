efbcbad67afa493c0f038b1f67d1e02ede6a6870
Bruno Montemartini <montemartinibruno@gmail.com>
2022-06-08 14:32:02 -0300

    deployment

--- myapp/libreria/admin.py
+++ myapp/libreria/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Libro
+from .models import Libro, Author
 # Register your models here.
 
 admin.site.register(Libro)
+admin.site.register(Author)
--- myapp/libreria/models.py
+++ myapp/libreria/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.forms import ImageField
 
-# Create your models here.
+
+
 class Libro(models.Model):
     
+    
+
     titulo = models.CharField(max_length=100, verbose_name='titulo')
     imagen = models.ImageField(upload_to='imagenes/', verbose_name='imagen', null=True)
     descripcion = models.CharField(max_length=500 ,verbose_name='descripcion', null=True)
